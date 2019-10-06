dcbc87a63c22086dca99fa576c066efbd623a3e3
Omar Quintero <omar.quintero.ms@gmail.com>
2019-10-05 22:42:28 -0500

    Material Design, Interfaz

---
+++ app/src/main/java/mx/tec/alarmate/EditAlarm.kt
@@ -0,0 +1,12 @@
+package mx.tec.alarmate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditAlarm : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_alarm)
+    }
+}
