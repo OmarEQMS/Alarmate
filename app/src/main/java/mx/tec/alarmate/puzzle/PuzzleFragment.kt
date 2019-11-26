038fc5a132ba7da6c09743fd90aca14ecfd65ad1
Abraham Torres <jabraham@gmail.com>
2019-11-25 21:04:50 -0600

    Alarm flash

--- app/src/main/java/mx/tec/alarmate/puzzle/PuzzleFragment.kt
+++ app/src/main/java/mx/tec/alarmate/puzzle/PuzzleFragment.kt
@@ -1,6 +1,9 @@
 package mx.tec.alarmate.puzzle
 
+import android.Manifest
+import android.app.AlertDialog
 import android.content.Context
+import android.content.pm.PackageManager
 import android.media.Ringtone
 import android.os.*
 import androidx.fragment.app.Fragment
@@ -10,15 +13,22 @@ import android.view.View
 import android.media.RingtoneManager
 import android.media.MediaPlayer
 import android.media.AudioAttributes
-
-
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.content.DialogInterface
+import android.hardware.camera2.CameraAccessException
+import androidx.core.content.ContextCompat.getSystemService
+import android.hardware.camera2.CameraManager
 
 open class PuzzleFragment(open val alarm: Alarm, open val puzzle: Puzzle?) : Fragment() {
     var listener: PuzzleListener? = null
     lateinit var vibrator: Vibrator
     lateinit var mediaPlayer: MediaPlayer
     var vibrating = false
-    val handler = Handler()
+    val vibrationHandler = Handler()
+    var flashing = false
+    var flashOn = false
+    val flashingHandler = Handler()
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -41,6 +51,24 @@ open class PuzzleFragment(open val alarm: Alarm, open val puzzle: Puzzle?) : Fra
     }
 
     open fun startAlarm(){
+        sound()
+
+        if(alarm.vibration){
+            vibrate()
+        }
+
+        if(alarm.flash){
+            flash()
+        }
+    }
+
+    open fun stopAlarm(){
+        mediaPlayer.stop()
+        vibrating = false
+        flashing = false
+    }
+
+    open fun sound(){
         var alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
 
         if (alert == null) {
@@ -57,23 +85,14 @@ open class PuzzleFragment(open val alarm: Alarm, open val puzzle: Puzzle?) : Fra
         mediaPlayer.setDataSource(context!!, alert)
         mediaPlayer.prepare()
         mediaPlayer.start()
-
-        if(alarm.vibration){
-            vibrate()
-        }
     }
 
-    open fun stopAlarm(){
-        mediaPlayer.stop()
-        vibrating = false
-    }
-
-    private fun vibrate(){
+    open fun vibrate(){
         if(vibrating){
             return
         }
         vibrating = true
-        handler.postDelayed(object : Runnable {
+        vibrationHandler.postDelayed(object : Runnable {
             override fun run() {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     vibrator.vibrate(VibrationEffect.createOneShot(VIBRATION_TIME, VibrationEffect.EFFECT_HEAVY_CLICK))
@@ -82,9 +101,75 @@ open class PuzzleFragment(open val alarm: Alarm, open val puzzle: Puzzle?) : Fra
                     vibrator.vibrate(VIBRATION_TIME)
                 }
                 if (vibrating) {
-                    handler.postDelayed(this, VIBRATION_PAUSE_TIME)
+                    vibrationHandler.postDelayed(this, VIBRATION_PAUSE_TIME)
                 }
             }
         }, VIBRATION_PAUSE_TIME)
+    }
+
+    open fun flash(){
+        if(flashing){
+            return
+        }
+        if (ContextCompat.checkSelfPermission(activity!!, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity!!,
+                    Manifest.permission.CAMERA)) {
+                AlertDialog.Builder(context)
+                    .setTitle("Permitir accesso a cámara")
+                    .setMessage("Es necesario utilizar el flash de la cámara para la alarma")
+                    .setPositiveButton(android.R.string.yes,
+                        DialogInterface.OnClickListener { dialog, which ->
+                            beginFlash()
+                        })
+                    .setNegativeButton(android.R.string.no, null)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show()
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(activity!!, arrayOf(Manifest.permission.CAMERA), MY_PERMISSIONS_REQUEST_CAMERA)
+            }
+        } else {
+            // Permission has already been granted
+            beginFlash()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == MY_PERMISSIONS_REQUEST_CAMERA){
+            beginFlash()
+        }
+    }
+
+    private fun beginFlash(){
+        if (ContextCompat.checkSelfPermission(activity!!, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity!!, arrayOf(Manifest.permission.CAMERA), MY_PERMISSIONS_REQUEST_CAMERA)
+            return
+        }
+
+        flashing = true
+        flashingHandler.postDelayed(object : Runnable {
+            override fun run() {
+                toggleFlash()
+                if (flashing) {
+                    flashingHandler.postDelayed(this, FLASHING_PAUSE_TIME)
+                }
+            }
+        }, FLASHING_PAUSE_TIME)
+    }
+
+    private fun toggleFlash(){
+        val cameraManager = context!!.getSystemService(Context.CAMERA_SERVICE) as CameraManager?
+        try {
+            val cameraId = cameraManager!!.cameraIdList[0]
+            flashOn = !flashOn
+            cameraManager.setTorchMode(cameraId, flashOn)
+        } catch (e: CameraAccessException) {
+        }
     }
 }
